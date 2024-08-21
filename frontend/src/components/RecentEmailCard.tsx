6de792658f1e55d92ce4b4aee28a04382484bf3b
Khanh Ha <nkhanhha26@gmail.com>
2024-08-20 23:45:46 -0400

    Adding Templates

--- frontend/src/components/RecentEmailCard.tsx
+++ frontend/src/components/RecentEmailCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Mail } from "lucide-react";
-import { CardFooter } from "../ui/card";
+import { CardFooter, Card, CardContent } from "../ui/card";
 import { useNavigate } from "react-router-dom";
 
 interface RecentEmailCardProps {
@@ -16,14 +16,20 @@ const RecentEmailCard: React.FC<RecentEmailCardProps> = ({ id, title }) => {
   };
 
   return (
-    <div className="flex flex-col items-center p-2">
-      <button
-        className="w-32 h-40 bg-white border border-gray-300 rounded-md shadow-sm flex flex-col items-center justify-center"
-        onClick={handleClick}
-      >
-        <Mail className="w-12 h-12 text-black hover:text-blue-500" />
-      </button>
-      <CardFooter className="text-center mt-2 text-sm">{title}</CardFooter>
+    <div className="flex flex-col items-center px-1">
+      <Card className="w-40 h-56">
+        <button
+          className="w-40 h-56 bg-white border border-gray-300 rounded-md shadow-sm flex flex-col items-center justify-center"
+          onClick={handleClick}
+        >
+          <CardContent className="items-center justify-center flex flex-col">
+            <Mail className="w-16 h-16 text-black hover:text-blue-500" />
+          </CardContent>
+        </button>
+        <CardFooter className="text-center mt-2 text-md text-white font-md">
+          {title}
+        </CardFooter>
+      </Card>
     </div>
   );
 };
