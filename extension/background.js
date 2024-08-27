bbfc8767cf37749842cd82b3ec8054871160cbf3
Khanh Ha <nkhanhha26@gmail.com>
2024-08-27 15:51:54 -0400

    Added summarizing and drafting features to popup

--- extension/background.js
+++ extension/background.js
@@ -2,20 +2,37 @@ let storedEmailData = null;
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   console.log("Message received in background script:", message);
+
+  // STORING EMAIL DATA
   if (message.action === "storeEmailData") {
     storedEmailData = message.emailData;
     console.log("Email data stored in background:", storedEmailData);
+    return; // Ensure the message listener finishes after storing data
   }
 
-  if (message.action === "generateEmailResponse") {
+  // NAVIGATE TO THE WEBSITE
+  if (message.action === "openDraftURL") {
+    chrome.tabs.create({ url: "http://localhost:5173" });
+    return;
+  }
+
+  // GENERATE RESPONSE
+  if (
+    message.action === "generateEmailResponse" ||
+    message.action === "summarizeEmailContent"
+  ) {
     if (!storedEmailData) {
       console.error("No email data stored");
       sendResponse({ error: "No email data provided" });
       return;
     }
 
-    // Pass the entire email data to the backend
-    fetch("http://localhost:5259/api/EmailResponseAPI/generate-email", {
+    const apiUrl =
+      message.action === "generateEmailResponse"
+        ? "http://localhost:5259/api/EmailResponseAPI/generate-email"
+        : "http://localhost:5259/api/EmailResponseAPI/summarize-email";
+
+    fetch(apiUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -40,7 +57,6 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
       })
       .catch((error) => {
         console.error("Error calling backend API:", error);
-        // Send an error message back to the popup
         chrome.runtime.sendMessage({
           action: "displayEmailResponse",
           response: "Error generating response: " + error.message,
